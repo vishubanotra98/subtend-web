b23af9f36a3a020e2fd96a81f2ed711ffb739bce
Vishu Banotra <111637458+VishuBanotra@users.noreply.github.com>
2026-04-03 13:56:36 +0530

    Added Workflow Settings

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { CreateWorkspaceModal } from "@/components/Forms/OnboardingForm";
 import { auth } from "@/lib/auth";
-import { LayoutGrid } from "lucide-react"; // Clean icon for Workspaces
+import { LayoutGrid } from "lucide-react";
 
 export default async function OnboardingPage() {
   const session = await auth();
